2288dc7f960e87f4cac8b95c6ea1b8afbe7438fc
Sembah <macbook@045.127.028.145.hva.nl>
2016-05-10 17:14:33 +0200

    Issue fixing
    Fixed issue #3 and #4

--- static/js/script.js
+++ static/js/script.js
@@ -1,50 +1,52 @@
-(function () {
-    'use strict';
+if ('querySelector' in document && 'addEventListener' in window && 'classList' in document.createElement('a') && document.createElement('audio').canPlayType){
     
-    var audios = document.querySelectorAll('audio');
-    var sections = document.querySelectorAll('section');
-    var handler;
-    var keys = {}
-    
-    function play(evt, btn) {
-        
-        if(this) btn = this;
-        
-        var audio = btn.parentNode.querySelector('audio');
+    (function () {
+        'use strict';
 
-        if(audio.paused){
-            
-            audio.play();
-            audio.currentTime = 0;
-            console.log(btn);
-            btn.classList.add('active');
-            return;
-        } 
-        
-        audio.pause();
-        btn.classList.remove('active');
-    };
+        var audios = document.querySelectorAll('audio');
+        var sections = document.querySelectorAll('section');
+        var handler;
+        var keys = {}
+
+        function play(evt, btn) {
+
+            if(this) btn = this;
+
+            var audio = btn.parentNode.querySelector('audio');
+
+            if(audio.paused){
+
+                audio.play();
+                audio.currentTime = 0;
+                btn.classList.add('active');
+                return;
+            } 
+
+            audio.pause();
+            btn.classList.remove('active');
+        };
+
+        window.addEventListener('keydown', function(evt) {
+            if(keys[evt.keyCode]) play(null, keys[evt.keyCode]);
+        });
+
+        if ('ontouchstart' in document.documentElement) handler = 'touchstart';
+        else handler = 'click';
+
+        for (var i = 0; i < sections.length; i += 1) {
+            var key = sections[i].querySelector('audio').getAttribute('data-key');
+            var keyString = String.fromCharCode(key);
+            var btn = document.createElement('button');
+            btn.innerHTML = sections[i].querySelector('h2').innerHTML + ' (' + keyString + ')';
+            sections[i].appendChild(btn);
+            btn.addEventListener(handler, play, false);
+            audios[i].removeAttribute('controls');
+            keys[key] = btn;
+        }
+
+        document.body.classList.add('enhanced-with-js')
+
+    }());
     
-    window.addEventListener('keydown', function(evt) {
-        if(keys[evt.keyCode]) play(null, keys[evt.keyCode]);
-    });
+}
     
-    if ('ontouchstart' in document.documentElement) handler = 'touchstart';
-    else handler = 'click';
-    
-    for (var i = 0; i < sections.length; i += 1) {
-        var key = sections[i].querySelector('audio').getAttribute('data-key');
-        var keyString = String.fromCharCode(key);
-        var btn = document.createElement('button');
-        btn.innerHTML = sections[i].querySelector('h2').innerHTML + ' (' + keyString + ')';
-        sections[i].appendChild(btn);
-        btn.addEventListener(handler, play, false);
-        audios[i].removeAttribute('controls');
-        //console.log(btn);
-        keys[key] = btn;
-        //console.log(keys[key]);
-    }
-    
-    document.body.classList.add('enhanced-with-js')
-    
-}());
